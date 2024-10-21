b63c74144ede08aa11de9ab0fd9e91c4cdcdf8fb
jacopolegovini <legovini96@gmail.com>
2024-10-21 16:26:19 +0200

    exercise completed

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,56 @@
+<?php
+$contacts = [
+    [
+        'name' => 'Luca Rossi',
+        'email' => 'luca.rossi@example.com',
+        'phone' => '+39 345 678 1234'
+    ],
+    [
+        'name' => 'Marta Bianchi',
+        'email' => 'marta.bianchi@example.com',
+        'phone' => '+39 331 567 8910'
+    ],
+    [
+        'name' => 'Giovanni Verdi',
+        'email' => 'giovanni.verdi@example.com',
+        'phone' => '+39 320 456 7890'
+    ],
+    [
+        'name' => 'Elena Neri',
+        'email' => 'elena.neri@example.com',
+        'phone' => '+39 347 123 4567'
+    ],
+    [
+        'name' => 'Paolo Moretti',
+        'email' => 'paolo.moretti@example.com',
+        'phone' => '+39 333 654 9876'
+    ]
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h2>
+            Contacts
+        </h2>
+    </header>
+    <main>
+        <ul>
+            @foreach ($contacts as $contact)
+                <li>
+                    {{$contact['name']}}
+                </li>
+            @endforeach
+        </ul>
+    </main>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Primi passi Laravel</h1>
+        <nav>
+            <a href="/contacts">Contacts</a>
+        </nav>
+    </header>
+    <main>Hello world</main>
+</body>
+</html>
